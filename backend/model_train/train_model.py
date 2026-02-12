43f7281ddf487230b3aaa77d4b5453ab9191aae9
Shubham Sati <shubham07sati@gmail.com>
2026-02-12 14:51:18 +0530

    Update train_model.py

--- backend/model_train/train_model.py
+++ backend/model_train/train_model.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
@@ -8,22 +7,25 @@ import joblib
 data = pd.read_csv("chikitsacloud_synthetic_medical_data.csv")
 
 
-X = data[['age', 'gender', 'height_cm', 'weight_kg', 'bmi', 
+X = data[['age', 'gender', 'height_cm', 'weight_kg', 'bmi',
           'systolic_bp', 'diastolic_bp', 'glucose', 'cholesterol']]
 y = data['risk_label']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-model.fit(X_train, y_train)
-print("Accuracy:", model.score(X_test, y_test))
 
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+
+model = RandomForestClassifier(n_estimators=100, random_state=42)
 
 print("Training Model...")
-model = RandomForestClassifier(n_estimators=100, random_state=42)
-model.fit(X, y)
+model.fit(X_train, y_train)
+
+
+accuracy = model.score(X_test, y_test)
+print("Accuracy:", accuracy)
 
 
 joblib.dump(model, "chikitsacloud_risk_model.pkl")
-
 print("✅ Model saved as 'chikitsacloud_risk_model.pkl'")
-
-
